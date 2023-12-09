dd51be27d45e05cdd511f51bf6a6150c7b3129d8
Giacomo Satta <giacomo.satta@healthdigitalplatform.onmicrosoft.com>
2023-12-09 16:45:49 +0100

    Aggiunto json con valori reali e aggiornato programma per inserirli nel db tramite dapper + metodo per gestire il single quote

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Data;
 using System.Globalization;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Dapper;
 using HelloWorld.Data;
 using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace HelloWorld
 {
@@ -18,99 +20,45 @@ namespace HelloWorld
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-
             DataContextDapper dapper = new DataContextDapper(config);
-            DataContextEF entityFramework = new DataContextEF(config);
+            string computersJson = File.ReadAllText("Computers.json");
 
-            DateTime rightNow = dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+            // JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // };
 
-            // Console.WriteLine(rightNow.ToString());
-            
-            Computer myComputer = new Computer() 
+            // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson,jsonOptions);
+            IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+
+
+
+            if (computers == null) return;
+
+            foreach (Computer computer in computers)
             {
-                Motherboard = "Z690",
-                HasWifi = true,
-                HasLTE = false,
-                ReleaseDate = DateTime.Now,
-                Price = 943.87m,
-                VideoCard = "RTX 2060"
-            };
-
-            entityFramework.Add(myComputer);
-            entityFramework.SaveChanges();
-
-            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                 string sql = @"INSERT INTO TutorialAppSchema.Computer (
                 Motherboard,
                 HasWifi,
                 HasLTE,
                 ReleaseDate,
                 Price,
                 VideoCard
-            ) VALUES ('" + myComputer.Motherboard 
-                    + "','" + myComputer.HasWifi
-                    + "','" + myComputer.HasLTE
-                    + "','" + myComputer.ReleaseDate.ToString("yyyy-MM-dd")
-                    + "','" + myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-                    + "','" + myComputer.VideoCard
+            ) VALUES ('" + EsacpeSingleQuote(computer.Motherboard) 
+                    + "','" + computer.HasWifi
+                    + "','" + computer.HasLTE
+                    + "','" + computer.ReleaseDate
+                    + "','" + computer.Price
+                    + "','" + EsacpeSingleQuote(computer.VideoCard)
             + "')";
 
-            // Console.WriteLine(sql);
-
-            // int result = dapper.ExecuteSqlWithRowCount(sql);
-            bool result = dapper.ExecuteSql(sql);
-
-            // Console.WriteLine(result);
-
-            string sqlSelect = @"
-            SELECT 
-                Computer.ComputerId,
-                Computer.Motherboard,
-                Computer.HasWifi,
-                Computer.HasLTE,
-                Computer.ReleaseDate,
-                Computer.Price,
-                Computer.VideoCard
-             FROM TutorialAppSchema.Computer";
-
-            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
-
-            Console.WriteLine("'ComputerId','Motherboard','HasWifi','HasLTE','ReleaseDate'" 
-                + ",'Price','VideoCard'");
-            foreach(Computer singleComputer in computers)
-            {
-                Console.WriteLine("'" + singleComputer.ComputerId 
-                    + "','" + singleComputer.Motherboard
-                    + "','" + singleComputer.HasWifi
-                    + "','" + singleComputer.HasLTE
-                    + "','" + singleComputer.ReleaseDate.ToString("yyyy-MM-dd")
-                    + "','" + singleComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-                    + "','" + singleComputer.VideoCard + "'");
+            dapper.ExecuteSql(sql);
             }
 
-            IEnumerable<Computer>? computersEf = entityFramework.Computer?.ToList<Computer>();
-
-            if (computersEf != null)
-            {
-                Console.WriteLine("'ComputerId','Motherboard','HasWifi','HasLTE','ReleaseDate'" 
-                    + ",'Price','VideoCard'");
-                foreach(Computer singleComputer in computersEf)
-                {
-                    Console.WriteLine("'" + singleComputer.ComputerId 
-                        + "','" + singleComputer.Motherboard
-                        + "','" + singleComputer.HasWifi
-                        + "','" + singleComputer.HasLTE
-                        + "','" + singleComputer.ReleaseDate.ToString("yyyy-MM-dd")
-                        + "','" + singleComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-                        + "','" + singleComputer.VideoCard + "'");
-                }
-            }
-
-            // myComputer.HasWifi = false;
-            // Console.WriteLine(myComputer.Motherboard);
-            // Console.WriteLine(myComputer.HasWifi);
-            // Console.WriteLine(myComputer.ReleaseDate);
-            // Console.WriteLine(myComputer.VideoCard);
         }
 
+        static string EsacpeSingleQuote(string input){
+            return input.Replace("'","''");
+        }
     }
 }
