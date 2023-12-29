6c14fdb8f2556e2fdd82852ed99dd2db6baf1bcf
Giacomo Satta <giacomo.satta@healthdigitalplatform.onmicrosoft.com>
2023-12-29 15:48:40 +0100

    aggiunta search dei posts

---
+++ DotNetAPI/Models/Dtos/PostToAddDto.cs
@@ -0,0 +1,7 @@
+namespace DotnetAPI.Models.Dtos;
+
+public partial class PostToAddDto
+{
+    public string PostTitle { get; set; } ="";
+    public string PostContent { get; set; }="";
+}
--- DotNetAPI/Models/Post.cs
+++ DotNetAPI/Models/Post.cs
@@ -1,6 +1,6 @@
 namespace DotnetAPI.Models;
 
-public partial class Posts
+public partial class Post
 {
     public int PostId { get; set; }
     public int UserId { get; set; }
